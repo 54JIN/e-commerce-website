3bc7f11c9e74121996e4c7e16acbcc09b4d169ae
54JIN <sajinsaju2002@gmail.com>
2024-07-15 01:11:14 -0400

    Added Estimate Section

--- src/Pages/Estimate/Estimate.js
+++ src/Pages/Estimate/Estimate.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
 import './Estimate.css';
 
 //Components
 import Header from '../../Assets/Components/HeaderSolid';
+import Footer from '../../Assets/Components/Footer';
 
 //Images
 import TudeluIntroVid from '../../Assets/Images/TudeluIntroVid.mp4';
 
 function Estimate() {
+    const [measurements, setMeasurements] = useState({ linearFt: null, linearIn: null, verticalFt: null, verticalIn: null })
+
+    const handleChange = (evt) => {
+        const { name, value } = evt.target;
+        
+        setMeasurements((prevData) => ({
+            ...prevData,
+            [name] : value
+        }))
+    }
+
   return (
     <div className="Estimate">
         <Header />
@@ -20,6 +34,62 @@ function Estimate() {
                 <video src={TudeluIntroVid} autoPlay loop muted disablePictureInPicture />
             </div>
         </div>
+        <div className="Estimate-Calculation-Wrapper">
+            <div className="Estimate-Calculation">
+                <h2>Give your dimensions</h2>
+                <div className='Estimate-Calculation-Content Calculation-Linear'>
+                    <p>The linear width of my opening is</p>
+                    <div className='Estimate-Calculation-Content-Input'>
+                        <div className='Estimate-Calculation-Content-Input-ft'>
+                            <input type="text" placeholder='0' value={measurements.linearFt} name="linearFt" onChange={handleChange} />
+                            <p>ft</p>
+                        </div>
+                        <div className='Estimate-Calculation-Content-Input-in'>
+                            <input type="text" placeholder='0' value={measurements.linearIn} name="linearIn" onChange={handleChange} />
+                            <p>in</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='Estimate-Calculation-Content Calculation-Vertical'>
+                    <p>The vertical height of my opening is</p>
+                    <div className='Estimate-Calculation-Content-Input'>
+                        <div className='Estimate-Calculation-Content-Input-ft'>
+                            <input type="text" placeholder='0' value={measurements.verticalFt} name="verticalFt" onChange={handleChange} />
+                            <p>ft</p>
+                        </div>
+                        <div className='Estimate-Calculation-Content-Input-in'>
+                            <input type="text" placeholder='0' value={measurements.verticalIn} name="verticalIn" onChange={handleChange} />
+                            <p>in</p>
+                        </div>
+                    </div>
+                </div>
+                <button>Get My Estimate</button>
+            </div>
+            <div className='Estimate-Result'>
+                <h2>Closure</h2>
+                <div className='Estimate-Result-Content'>
+                    <div className='Estimate-Result-Content-Description'>
+                        <p>Tudelü: Closure is the easiest way to beautify your space while creating the calm, focused environment you need for any task.</p>
+                    </div>
+                    <div className='Estimate-Result-Content-Details'>
+                        <div>
+                            <h3>Closure is</h3>
+                            <p>Retractable</p>
+                            <p>Occlusive</p>
+                            <p>Remote-controlled</p>
+                        </div>
+                        <div>
+                            <h3>Closure is not:</h3>
+                            <p>Sound proof</p>
+                        </div>
+                    </div>
+                    <div className='Estimate-Result-Content-Cost'>
+                        <h3>$7,000</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer />
     </div>
   );
 }
